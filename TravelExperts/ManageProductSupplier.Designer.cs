1e5f42d5a2a316032e140001afdc4f10228cbc32
huijillain <skyjillain@gmail.com>
2020-10-09 00:55:19 -0600

    update on ProductSupplier

--- TravelExperts/ManageProductSupplier.Designer.cs
+++ TravelExperts/ManageProductSupplier.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAddProductSupplier = new System.Windows.Forms.Label();
+            this.gvProdSupplierList = new System.Windows.Forms.DataGridView();
+            this.btnAddProdSupplier = new System.Windows.Forms.Button();
+            this.btnEditProdSupplier = new System.Windows.Forms.Button();
+            this.btnDeleteProdSupplier = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvProdSupplierList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAddProductSupplier
+            // 
+            this.lblAddProductSupplier.AutoSize = true;
+            this.lblAddProductSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddProductSupplier.Location = new System.Drawing.Point(12, 28);
+            this.lblAddProductSupplier.Name = "lblAddProductSupplier";
+            this.lblAddProductSupplier.Size = new System.Drawing.Size(254, 25);
+            this.lblAddProductSupplier.TabIndex = 0;
+            this.lblAddProductSupplier.Text = "Mange Product Suppliers";
+            // 
+            // gvProdSupplierList
+            // 
+            this.gvProdSupplierList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvProdSupplierList.Location = new System.Drawing.Point(17, 76);
+            this.gvProdSupplierList.Name = "gvProdSupplierList";
+            this.gvProdSupplierList.Size = new System.Drawing.Size(223, 338);
+            this.gvProdSupplierList.TabIndex = 1;
+            // 
+            // btnAddProdSupplier
+            // 
+            this.btnAddProdSupplier.Location = new System.Drawing.Point(306, 76);
+            this.btnAddProdSupplier.Name = "btnAddProdSupplier";
+            this.btnAddProdSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnAddProdSupplier.TabIndex = 2;
+            this.btnAddProdSupplier.Text = "Add";
+            this.btnAddProdSupplier.UseVisualStyleBackColor = true;
+            this.btnAddProdSupplier.Click += new System.EventHandler(this.btnAddProdSupplier_Click);
+            // 
+            // btnEditProdSupplier
+            // 
+            this.btnEditProdSupplier.Location = new System.Drawing.Point(306, 233);
+            this.btnEditProdSupplier.Name = "btnEditProdSupplier";
+            this.btnEditProdSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnEditProdSupplier.TabIndex = 3;
+            this.btnEditProdSupplier.Text = "Edit";
+            this.btnEditProdSupplier.UseVisualStyleBackColor = true;
+            this.btnEditProdSupplier.Click += new System.EventHandler(this.btnEditProdSupplier_Click);
+            // 
+            // btnDeleteProdSupplier
+            // 
+            this.btnDeleteProdSupplier.Location = new System.Drawing.Point(306, 390);
+            this.btnDeleteProdSupplier.Name = "btnDeleteProdSupplier";
+            this.btnDeleteProdSupplier.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteProdSupplier.TabIndex = 4;
+            this.btnDeleteProdSupplier.Text = "Delete";
+            this.btnDeleteProdSupplier.UseVisualStyleBackColor = true;
+            this.btnDeleteProdSupplier.Click += new System.EventHandler(this.btnDeleteProdSupplier_Click);
+            // 
+            // ManageProductSupplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ManageProductSupplier";
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.btnDeleteProdSupplier);
+            this.Controls.Add(this.btnEditProdSupplier);
+            this.Controls.Add(this.btnAddProdSupplier);
+            this.Controls.Add(this.gvProdSupplierList);
+            this.Controls.Add(this.lblAddProductSupplier);
+            this.Name = "ManageProductSupplier";
+            this.Text = "Manage Product Supplier";
+            this.Load += new System.EventHandler(this.ManageProductSupplier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvProdSupplierList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAddProductSupplier;
+        private System.Windows.Forms.DataGridView gvProdSupplierList;
+        private System.Windows.Forms.Button btnAddProdSupplier;
+        private System.Windows.Forms.Button btnEditProdSupplier;
+        private System.Windows.Forms.Button btnDeleteProdSupplier;
     }
 }
--- TravelExperts/ManageProductSupplier.cs
+++ TravelExperts/ManageProductSupplier.cs
@@ -1,20 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TravelExperts
 {
     public partial class ManageProductSupplier : Form
     {
+        private readonly TravelExpertsEntities _db;
         public ManageProductSupplier()
         {
             InitializeComponent();
+            _db = new TravelExpertsEntities();
+        }
+
+        private void ManageProductSupplier_Load(object sender, EventArgs e)
+        {
+            PopulateGrid();
+        }
+        //Function to PopulateGrid. Can be called anytime we need a grid refresh   
+        public void PopulateGrid()
+        {
+            //Select ProductSupplier as ProdName from Product Table
+            var productsupplier = _db.Products.Select(p => new
+            {
+                ProductSupplier = p.ProdName,
+                p.ProductId,
+                p.SupplierId
+            })
+                .ToList();
+
+            gvProdSupplierList.DataSource = productsupplier;
+            gvProdSupplierList.Columns["ProductId"].Visible = false;
+        }
+
+        private void btnAddProdSupplier_Click(object sender, EventArgs e)
+        {
+            var addEditProductSupplier = new AddEditProductSupplier(this);
+
+            addEditProductSupplier.MdiParent = this.MdiParent;
+            addEditProductSupplier.Show();
+        }
+
+        private void btnEditProdSupplier_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //select row          
+                var id = Convert.ToString(gvProdSupplierList.CurrentRow.Cells[0].Value);
+
+                //query database for record                
+                var productsupplier = _db.Products.FirstOrDefault(p => p.ProdName == id);
+
+                //launch AddEditProduct window with Data
+                var addEditProductSupplier = new AddEditProductSupplier(productsupplier, this);
+                addEditProductSupplier.MdiParent = this.MdiParent;
+                addEditProductSupplier.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void btnDeleteProdSupplier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //select row
+                var id = Convert.ToString(gvProdSupplierList.CurrentRow.Cells[0].Value);
+
+                //query database for record
+                var product = _db.Products.FirstOrDefault(p => p.ProdName == id);
+
+                DialogResult dr = MessageBox.Show("Are You Sure You Want To Delete This Product?",
+                        "Delete", MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.Yes)
+                {
+                    //delete Product from table
+                    _db.Products.Remove(product);
+                    _db.SaveChanges();
+                }
+                PopulateGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+            //gvProductList.Refresh();
         }
     }
 }
